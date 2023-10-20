1cb3270fee87e23ae26f5083ee65c161a4426437
Scott Grunerud <scott@sparelabs.com>
2023-10-20 09:24:04 -0700

    Working MVP of splash screen vs update race condition

--- App.tsx
+++ App.tsx
@@ -1,9 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import * as Updates from "expo-updates";
-import { useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
+import * as SplashScreen from "expo-splash-screen";
 
 export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
+
   async function onFetchUpdateAsync() {
     try {
       const update = await Updates.checkForUpdateAsync();
@@ -19,13 +22,40 @@ export default function App() {
   }
 
   useEffect(() => {
-    console.log("FETCHING UPDATE");
-    onFetchUpdateAsync();
-    console.log("AFTER FETCHING UPDATE");
+    async function prepare() {
+      try {
+        // Pre-load fonts, make any API calls you need to do here
+        // Artificially delay for two seconds to simulate a slow loading
+        // experience. Please remove this if you copy and paste the code!
+        onFetchUpdateAsync();
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Tell the application to render
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
   }, []);
 
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      // This tells the splash screen to hide immediately! If we call this after
+      // `setAppIsReady`, then we may see a blank screen while the app is
+      // loading its initial state and rendering its first pixels. So instead,
+      // we hide the splash screen once we know the root view has already
+      // performed layout.
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
+    <View style={styles.container} onLayout={onLayoutRootView}>
       <Text>
         This app was built on:
         {new Date().toLocaleDateString()} at {new Date().toLocaleTimeString()}
@@ -38,7 +68,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "red",
     alignItems: "center",
     justifyContent: "center",
   },
